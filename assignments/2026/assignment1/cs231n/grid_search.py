27ab8ee8ce6d21158073b8f1c4cdc4ed42be7c85
Void User <amshulha@ualberta.ca>
2026-04-27 13:49:09 -0600

    giving back dataset

--- assignments/2026/assignment1/cs231n/grid_search.py
+++ assignments/2026/assignment1/cs231n/grid_search.py
@@ -9,3 +9,15 @@ def refine_grid(g,scale,num_step):
 
 def cv_cube(g1,g2,num_iter):
     return ([(x,y) for x in g1 for y in g2], num_iter)
+
+def run_SGD(num_iter):
+    pass
+
+grid_steps = 10
+num_top_values = 5
+num_iter = 100
+
+def grid_search():
+    results = dict.fromkeys(cv_cube(g1,g2,num_iter)[0])
+    for step in range(grid_steps):
+        results = run_SGD(num_iter)
